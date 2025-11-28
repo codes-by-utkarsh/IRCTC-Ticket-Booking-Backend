fd9c903023f9cd70750e9ac016c3fc71df78cafb
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-11-28 19:56:12 +0530

    made the file and fixed final statement

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -7,9 +7,10 @@ import java.io.File;
 public class UserBookingService
 {
     private User user;
+    private static final String USER_PATH = "../localDb/users.json";
     public UserBookingService(User user1)
     {
         this.user = user1;
-        File
+        File users = new File(USER_PATH);
     }
 }
