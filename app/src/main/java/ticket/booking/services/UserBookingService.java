3a9d935534682fa78b118bff1c1e33ee8fedaaec
codes-by-utkarsh <usrivastava2011@gmail.com>
2025-11-28 19:44:27 +0530

    call

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,6 +1,12 @@
 package ticket.booking.services;
 
+import ticket.booking.entities.User;
+
 public class UserBookingService
 {
+    private User user;
+    public UserBookingService()
+    {
 
+    }
 }
