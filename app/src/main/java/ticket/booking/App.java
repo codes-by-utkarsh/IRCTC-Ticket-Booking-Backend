d40202c7aab0dc35f47e1ad5646205bb38283f3b
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-11-29 21:30:28 +0530

    main method

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -3,15 +3,22 @@
  */
 package ticket.booking;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import ticket.booking.services.UserBookingService;
+
+import java.util.Scanner;
+
 
 public class App
 {
     public static void main(String[] args)
     {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        List<Integer> list1 = list.stream().filter(n -> n%2 == 0).toList();
+        System.out.println("Command Line IRCTC");
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        UserBookingService userBookingService;
+        try
+        {
+            userBookingService = new UserBookingService();
+        }
     }
 }
