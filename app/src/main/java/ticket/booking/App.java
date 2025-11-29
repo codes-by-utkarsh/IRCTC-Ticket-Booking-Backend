35b95bec5cdce60a254a29516a017b0ae856d7d4
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-11-29 17:57:55 +0530

    fixed minor bugs

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -3,10 +3,15 @@
  */
 package ticket.booking;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App
 {
     public static void main(String[] args)
     {
-
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> list1 = list.stream().filter(n -> n%2 == 0).toList();
     }
 }
